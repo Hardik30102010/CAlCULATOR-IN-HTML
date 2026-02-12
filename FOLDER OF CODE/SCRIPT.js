75860994f7f11110610da1815f63200ce18ed7ca
HARDIK AWANA <hardikawana31@gmail.com>
2026-02-12 09:28:53 +0530

    Update SCRIPT.js

--- FOLDER OF CODE/SCRIPT.js
+++ FOLDER OF CODE/SCRIPT.js
@@ -2,3 +2,9 @@ function showpage(id) {
     document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
     document.getElementById(id).classList.add('active');
 }
+function append(value) {
+            document.getElementById('display').value += value;
+                }
+function clearDisplay() {
+            document.getElementById('display').value = "";
+        }
